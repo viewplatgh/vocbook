ed02654e0f37ab8066738c7d2a9af5c31b5a3866
Rob L <viewpl@gmail.com>
2019-01-13 21:47:33 +1100

    Use next/babel

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  // transform: {
-  //   "^.+\\.tsx?$": "ts-jest"
-  // },
   testMatch: ["**/*.(test|spec).(js|jsx|ts|tsx)"],
-  // globals: {
-  //   "ts-jest": {
-  //     useBabelrc: true,
-  //     tsConfigFile: "jest.tsconfig.json"
-  //   }
-  // },
-  coveragePathIgnorePatterns: ["/node_modules/", "enzyme.js"],
-  setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
+  coveragePathIgnorePatterns: ["/node_modules/", "jest.setup.js"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupTestFrameworkScriptFile: "<rootDir>/jest.setup.js",
   coverageReporters: ["json", "lcov", "text", "text-summary"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
